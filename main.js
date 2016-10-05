24213d746fa1898ee23b7ad55eb55bac76bfe45c
tinylittleburger <tinylittleburger@gmail.com>
2016-10-05 21:32:05 +0200

    Periodically checking for navigation header

--- main.js
+++ main.js
@@ -1,17 +1,35 @@
-var header = document.getElementsByClassName("header-main-navigation");
+function poll() {
+	var menu = getMenu();
 
-if (header.length > 0) {
-	var menu = header[0].getElementsByClassName("navigation-list");
-
-	if (menu.length > 0) {
-		var item = document.createElement("li");
-		item.className = "navigation-list__item";
-
-		var link = document.createElement("a");
-		link.className = "ember-view rt-link-to header-navigation__link";
-		link.innerHTML = "Export";
-
-		item.appendChild(link);
-		menu[0].appendChild(item);
+	if (menu) {
+		inject(menu);
+	} else {
+		setTimeout(poll, 100);
 	}
+}
+
+poll();
+
+function getMenu() {
+	var header = document.getElementsByClassName("header-main-navigation");
+
+	if (header.length > 0) {
+		var menu = header[0].getElementsByClassName("navigation-list");
+
+		if (menu.length > 0) {
+			return menu[0];
+		}
+	}
+}
+
+function inject(menu) {
+	var item = document.createElement("li");
+	item.className = "navigation-list__item";
+
+	var link = document.createElement("a");
+	link.className = "ember-view rt-link-to header-navigation__link";
+	link.innerHTML = "Export";
+
+	item.appendChild(link);
+	menu.appendChild(item);
 }
